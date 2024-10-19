65eb30f217b6f32c90887ee7cebffdf0ec6dcbf1
1123001 - Kezia Natalia <163112818+Kezia2212@users.noreply.github.com>
2024-10-19 21:21:26 +0700

    beneran terbaru

---
+++ model/user.java
@@ -0,0 +1,49 @@
+package model;
+
+abstract class User {
+    private String nama, alamat, ttl, telp;
+
+    public User(String nama, String alamat, String ttl, String telp) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.ttl = ttl;
+        this.telp = telp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(String ttl) {
+        this.ttl = ttl;
+    }
+
+    public String getTelp() {
+        return telp;
+    }
+
+    public void setTelp(String telp) {
+        this.telp = telp;
+    }
+
+}
+
+public class user {
+    
+}
